9cc2aabc3808dd4080084492c9594ad7e2ed8fe2
Smart Home Cambodia <tekdara@me.com>
2023-07-22 21:16:38 +0700

    update

---
+++ SimpleScheduler/rootfs/simplescheduler/scheduler.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+#  
+# Home Assistant Add-on: SimpleScheduler
+#  
+# ==============================================================================
+
+bashio::log.info "Running scheduler.sh"
+
+python3 /simplescheduler/scheduler.py
